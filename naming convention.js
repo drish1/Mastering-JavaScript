6cd190e2d9bec37c1490c51ae465649c8acd6240
Drishti Sharma <68895778+drish1@users.noreply.github.com>
2024-01-23 14:29:04 +0530

    Add files via upload

---
+++ naming convention.js
@@ -0,0 +1,18 @@
+//a variable name can have alphabets, numbers and two special symbol can be there $ and _
+//we can have only _ and $ as variable name too
+let _;
+_ = 1;
+console.log(_);
+//underscore can be used to separate segment of variable name
+let user_id;
+user_id = 100;
+console.log(user_id);
+//numbers can be used in variable name
+let rollno1 = 1;
+console.log(rollno1);
+//cameCase convention preferred when variable name consist multiple names 
+//in camelcasing the first word of variable name is always small and every other word starts with a capital letter
+let userName;
+userName = "Drishti";
+console.log(userName);
+// a variable name should not start with a number
